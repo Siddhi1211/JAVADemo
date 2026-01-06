e1f41cb078e9727c4e6690699d72943a36d4b4ce
Raghunandan Mundada <mundadaraghunandan@gmail.com>
2026-01-06 17:24:01 +0530

    Added all files 1st time from neha branch

---
+++ src/practiceJAVA/customExceptionNeha2.java
@@ -0,0 +1,23 @@
+package practiceJAVA;
+
+public class customExceptionNeha2 {
+
+	public static void checkAge(int age) throws customExceptionNeha
+	{
+		if(age>12)
+			throw new customExceptionNeha("Age is greater than 12");
+	}
+	public static void main(String[] args) {
+		try {
+		checkAge(16);
+		System.out.println("In try block");
+		}
+		catch(customExceptionNeha e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+
+	}
+
+}
